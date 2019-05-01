39f197fdc08d4b6907262203dfebd5f8a55a28bc
Oim <trustoim@gmail.com>
2019-05-01 17:33:58 +0700

    updated source

--- index.php
+++ index.php
@@ -1,35 +1,41 @@
 <html>
  <head>
  <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+ <link href="style.css" rel="stylesheet" type="text/css">
+ <script src="style.js" type="text/javascript"></script>
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
- <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
- </form>
+
+ <div class="user">
+    <header class="user__header">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg" alt="" />
+        <h1 class="user__title">REGISTER FORM</h1>
+    </header>
+    
+    <form class="form" action="index.php" method="post" enctype="multipart/form-data">
+        <div class="form__group">
+            <input type="text" placeholder="Name" name="name" class="form__input" />
+        </div>
+        
+        <div class="form__group">
+            <input type="email" placeholder="Email" name="email" class="form__input" />
+        </div>
+        
+        <div class="form__group">
+            <input type="text" placeholder="Job" name="job" class="form__input" />
+        </div>
+        
+        <button class="btn-blue" type="submit" name="submit">Register</button>
+        <button class="btn-gray" type="submit" name="load_data" >Load Data</button>
+    </form>
+</div>
+
+
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "oimappserver.database.windows.net";
+    $user = "oimtrust";
+    $pass = "Rahasia123!";
+    $db = "db_oimapp";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
@@ -45,7 +51,7 @@
             $job = $_POST['job'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO Registration (name, email, job, date) 
+            $sql_insert = "INSERT INTO users (name, email, job, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -60,7 +66,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM Registration";
+            $sql_select = "SELECT * FROM users";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
---
+++ style.js
@@ -0,0 +1,7 @@
+const buttonBlue = document.querySelector('.btn-blue')
+const buttonGray = document.querySelector('.btn-gray')
+const form   = document.querySelector('.form')
+
+button.addEventListener('click', function() {
+   form.classList.add('form--no') 
+});
