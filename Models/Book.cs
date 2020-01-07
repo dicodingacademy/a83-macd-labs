489caaebf898e46bc8e166602922b16488173307
Widyarso Joko Purnomo <widyjp10@gmail.com>
2020-01-07 11:23:11 +0700

    add Book model

---
+++ Models/Book.cs
@@ -0,0 +1,12 @@
+namespace BooksCatalogueAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Synopsis { get; set; }
+        public int ReleaseYear { get; set; }
+        public string CoverURL { get; set; }
+    }
+}
